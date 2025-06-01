44dcc2469267f74e1ad7d64f5da81172fb48a17a
Yasmin <78688354+yasminway@users.noreply.github.com>
2025-05-31 22:27:49 -0300

    Update igdb_proxy_all_fields.js

--- igdb_proxy_all_fields.js
+++ igdb_proxy_all_fields.js
@@ -54,5 +54,5 @@ app.get('/games', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(\`Servidor rodando na porta \${PORT}\`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
